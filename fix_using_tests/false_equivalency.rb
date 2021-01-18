e5128a27dce0933365aab9f42f8d0d4464320449
Ian Davies <mr.daviesian@gmail.com>
2021-01-18 21:38:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,9 +14,9 @@ end
 def selection(num)
   if num == 1
     binding.pry
-    "YUM YUM MUNCH MUNCH MUNCH"
+   p "YUM YUM MUNCH MUNCH MUNCH"
   else if num == 2
-    "HAM HAM HAM IN MY TUMMY"
+   p "HAM HAM HAM IN MY TUMMY"
   end
 end
 
