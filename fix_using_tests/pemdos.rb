708baaa943da656707ca016994c6874139951fcc
Ian Davies <mr.daviesian@gmail.com>
2021-01-18 20:55:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,8 +3,10 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string.to_i
+    10 * "s" + string
   else
     string
   end
 end
+
+puts (1 + number.to_i).to_s + ' is way better'
